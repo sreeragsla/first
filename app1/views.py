dedbb25616f8271ffb96a775e057fc79e39b544a
sreeragsla <you@example.com>
2024-11-22 19:07:48 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def sreerag1(request):
+    return HttpResponse('Sreerag Is A Python Fullstack Developer')
